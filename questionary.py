cace3acc91c99363f7ca10c0ccee8ec32101ebca
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2024-11-13 12:03:16 -0300

    Add validation

--- questionary.py
+++ questionary.py
@@ -1,7 +1,27 @@
+from inquirer import errors
 import inquirer
 
+
+def validate_product(answer, current):
+    if not current:
+        raise errors.ValidationError('', reason='É obrigatório informar o produto desejado!')
+
+    return True
+
+
+def validate_installment(answer, current):
+    if not current:
+        raise errors.ValidationError('', reason='É obrigatório informar a quantidade de parcela(s)!')
+    elif not current.isnumeric():
+        raise errors.ValidationError('', reason='Inserir apenas número(s)!')
+    elif not int(current) > 1 or not int(current) < 13:
+        raise errors.ValidationError('', reason='Informar a quantidade de parcelas entre 1 e 12!')
+
+    return True
+
+
 listFilter = [
-    inquirer.Text('product', 'Qual produto deseja procurar?'),
+    inquirer.Text('product', message='Qual produto deseja procurar?',validate=validate_product),
     inquirer.Text('productBrand', 'Qual a marca do produto deseja procurar?'),
     inquirer.Text('maxPrice', 'Qual o valor máximo aceitável (R$)?'),
     inquirer.List(
@@ -13,7 +33,9 @@ listFilter = [
     inquirer.Text(
         'installment',
         message='Deseja parcelar em até quantas vezes (Entre 1 e 12)?',
-        ignore=lambda x: x['typePayment'] != 'Crédito'),
+        ignore=lambda x: x['typePayment'] != 'Crédito',
+        validate=validate_installment
+    ),
     inquirer.List(
         'isShippingFree',
         message='É importante possuir frete grátis?',
